cc3bd9e5aeb845381ee0be7b371ecf2d7327234b
Harshit Jain <harshitjain0502@gmail.com>
2025-01-02 18:10:49 +0530

    add resources

--- generate_readme.py
+++ generate_readme.py
@@ -2,7 +2,6 @@ import os
 
 def generate_readme():
     readme_content = "# CTFLearn Solutions\n\n"
-    profile_url = "https://ctflearn.com/user/harshit_jain52"  # Update your profile link as needed
 
     # Traverse directories and collect files
     solutions = []
@@ -17,13 +16,23 @@ def generate_readme():
     # Sort solutions by numeric ID
     solutions.sort()
 
-    # Add solutions to README
     for solution_id, relative_path in solutions:
         readme_content += f"- [{solution_id}](./{relative_path})\n"
     
     # Add profile link
+    profile_url = "https://ctflearn.com/user/harshit_jain52"
     readme_content += f"\n[Profile]({profile_url})\n"
 
+     # Add Resources section
+    resources_section = """
+## Resources & Tools
+
+- [CTFLearn](https://ctflearn.com)
+- [CTF Handbook](https://ctf101.org/)
+- [Online decryption and decoding](https://cryptii.com/)
+"""
+    readme_content += resources_section
+    
     # Write to README.md
     with open("README.md", "w") as readme_file:
         readme_file.write(readme_content)
